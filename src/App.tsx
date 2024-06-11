0ffb6bd2ecabb1c0de32bc405f5a829c3e6ce83d
lilzem <dmytrynskiy74@gmail.com>
2024-06-12 00:19:26 +0300

    added get requests, axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,62 @@
 import styles from '@src/App.module.scss';
 
-import { useState } from 'react';
-import Header from './components/Header';
+import { useEffect, useRef, useState } from 'react';
+import { Radio, TextField } from '@mui/material';
+
+import logoIcon from '@src/assets/images/logo_with_name.svg';
 import Button from './components/Button';
 import Card from './components/Card';
-import { FormControlLabel, Input, Radio, TextField } from '@mui/material';
+import { User } from './interfaces/User';
+import { getUsers } from './api/api';
+import { USER_COUNT } from './constants';
 
 const App = () => {
-    console.log('hello people');
+    const usersRef = useRef<HTMLDivElement | null>(null);
+    const signUpRef = useRef<HTMLDivElement | null>(null);
+
+    const [users, setUsers] = useState<Array<User>>([]);
+    const [page, setPage] = useState<number>(1);
+
+    const usersPaginate = () => {
+        setPage((prev) => prev + 1);
+
+        getUsers(page, USER_COUNT)
+            .then((res) => setUsers((prev) => [...prev, res.users]))
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        getUsers(page, USER_COUNT)
+            .then((res) => setUsers(res.users))
+            .catch((err) => console.log(err));
+    }, []);
 
     return (
         <>
-            <Header />
+            <header>
+                <div className={styles.container}>
+                    <img src={logoIcon} alt="logo" />
+
+                    <nav>
+                        <Button
+                            text="Users"
+                            onClick={() => {
+                                usersRef.current?.scrollIntoView({
+                                    behavior: 'smooth',
+                                });
+                            }}
+                        />
+                        <Button
+                            text="Sign Up"
+                            onClick={() => {
+                                signUpRef.current?.scrollIntoView({
+                                    behavior: 'smooth',
+                                });
+                            }}
+                        />
+                    </nav>
+                </div>
+            </header>
 
             <main>
                 <div className={styles.info_block}>
@@ -24,62 +69,31 @@ const App = () => {
                             Development keeps evolving.
                         </p>
 
-                        <Button style={{ width: '100px', marginTop: '13px' }} text="Sign Up" />
+                        <Button
+                            style={{ width: '100px', marginTop: '13px' }}
+                            text="Sign Up"
+                            onClick={() => {
+                                signUpRef.current?.scrollIntoView({
+                                    behavior: 'smooth',
+                                });
+                            }}
+                        />
                     </div>
                 </div>
 
-                <div className={styles.get_block}>
+                <div className={styles.get_block} ref={usersRef}>
                     <h1>Working with GET request</h1>
 
                     <div className={styles.card_list}>
-                        <Card
-                            photo="https://s3-alpha-sig.figma.com/img/a120/4546/b321b7fbb0fb4c239e8a4c33e7af0e99?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AEBbNRyQXJAxZHE3G3798i-~hLSiDpHTzvTT-5P9rYRhUSiB9rHgwcrltxNhi3PCeLwJiLGsgTPfQyqGKqtSZ7TTEBK5YuEsBHJF0nAMBrOakcVKa2HWHb-xXb34gygN9z0Wj~Y6iRbdmklBJDIkcJmtRLV~DB3xmV7FY5vTLdiSw~cIkWslr0i6OHbc564PkBeBjJeFBeD7xK8z2tGVUJGqHDHS3kk2BLaJo7fCrGPK3BK9DBAH9BLqae1uyHgH6YIRupbFPne3wEjzafcmttDgVcER4tRH4WNkwG2MuVxy2P6uO990lXi5XFtMcs7X9DdngkIbz9O8yUrFzO16zQ__"
-                            name="Alexandre"
-                            position="Lead Independent Director"
-                            email="Alexandr_develop@gmail.com"
-                            phone="+38 (098) 198 44 24"
-                        />
-                        <Card
-                            photo="https://s3-alpha-sig.figma.com/img/a120/4546/b321b7fbb0fb4c239e8a4c33e7af0e99?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AEBbNRyQXJAxZHE3G3798i-~hLSiDpHTzvTT-5P9rYRhUSiB9rHgwcrltxNhi3PCeLwJiLGsgTPfQyqGKqtSZ7TTEBK5YuEsBHJF0nAMBrOakcVKa2HWHb-xXb34gygN9z0Wj~Y6iRbdmklBJDIkcJmtRLV~DB3xmV7FY5vTLdiSw~cIkWslr0i6OHbc564PkBeBjJeFBeD7xK8z2tGVUJGqHDHS3kk2BLaJo7fCrGPK3BK9DBAH9BLqae1uyHgH6YIRupbFPne3wEjzafcmttDgVcER4tRH4WNkwG2MuVxy2P6uO990lXi5XFtMcs7X9DdngkIbz9O8yUrFzO16zQ__"
-                            name="Alexandre"
-                            position="Lead Independent Director"
-                            email="Alexandr_develop@gmail.com"
-                            phone="+38 (098) 198 44 24"
-                        />
-                        <Card
-                            photo="https://s3-alpha-sig.figma.com/img/a120/4546/b321b7fbb0fb4c239e8a4c33e7af0e99?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AEBbNRyQXJAxZHE3G3798i-~hLSiDpHTzvTT-5P9rYRhUSiB9rHgwcrltxNhi3PCeLwJiLGsgTPfQyqGKqtSZ7TTEBK5YuEsBHJF0nAMBrOakcVKa2HWHb-xXb34gygN9z0Wj~Y6iRbdmklBJDIkcJmtRLV~DB3xmV7FY5vTLdiSw~cIkWslr0i6OHbc564PkBeBjJeFBeD7xK8z2tGVUJGqHDHS3kk2BLaJo7fCrGPK3BK9DBAH9BLqae1uyHgH6YIRupbFPne3wEjzafcmttDgVcER4tRH4WNkwG2MuVxy2P6uO990lXi5XFtMcs7X9DdngkIbz9O8yUrFzO16zQ__"
-                            name="Alexandre"
-                            position="Lead Independent Director"
-                            email="Alexandr_develop@gmail.com"
-                            phone="+38 (098) 198 44 24"
-                        />
-                        <Card
-                            photo="https://s3-alpha-sig.figma.com/img/a120/4546/b321b7fbb0fb4c239e8a4c33e7af0e99?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AEBbNRyQXJAxZHE3G3798i-~hLSiDpHTzvTT-5P9rYRhUSiB9rHgwcrltxNhi3PCeLwJiLGsgTPfQyqGKqtSZ7TTEBK5YuEsBHJF0nAMBrOakcVKa2HWHb-xXb34gygN9z0Wj~Y6iRbdmklBJDIkcJmtRLV~DB3xmV7FY5vTLdiSw~cIkWslr0i6OHbc564PkBeBjJeFBeD7xK8z2tGVUJGqHDHS3kk2BLaJo7fCrGPK3BK9DBAH9BLqae1uyHgH6YIRupbFPne3wEjzafcmttDgVcER4tRH4WNkwG2MuVxy2P6uO990lXi5XFtMcs7X9DdngkIbz9O8yUrFzO16zQ__"
-                            name="Alexandre"
-                            position="Lead Independent Director"
-                            email="Alexandr_develop@gmail.com"
-                            phone="+38 (098) 198 44 24"
-                        />
-                        <Card
-                            photo="https://s3-alpha-sig.figma.com/img/a120/4546/b321b7fbb0fb4c239e8a4c33e7af0e99?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AEBbNRyQXJAxZHE3G3798i-~hLSiDpHTzvTT-5P9rYRhUSiB9rHgwcrltxNhi3PCeLwJiLGsgTPfQyqGKqtSZ7TTEBK5YuEsBHJF0nAMBrOakcVKa2HWHb-xXb34gygN9z0Wj~Y6iRbdmklBJDIkcJmtRLV~DB3xmV7FY5vTLdiSw~cIkWslr0i6OHbc564PkBeBjJeFBeD7xK8z2tGVUJGqHDHS3kk2BLaJo7fCrGPK3BK9DBAH9BLqae1uyHgH6YIRupbFPne3wEjzafcmttDgVcER4tRH4WNkwG2MuVxy2P6uO990lXi5XFtMcs7X9DdngkIbz9O8yUrFzO16zQ__"
-                            name="Alexandre"
-                            position="Lead Independent Director"
-                            email="Alexandr_develop@gmail.com"
-                            phone="+38 (098) 198 44 24"
-                        />
-                        <Card
-                            photo="https://s3-alpha-sig.figma.com/img/a120/4546/b321b7fbb0fb4c239e8a4c33e7af0e99?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AEBbNRyQXJAxZHE3G3798i-~hLSiDpHTzvTT-5P9rYRhUSiB9rHgwcrltxNhi3PCeLwJiLGsgTPfQyqGKqtSZ7TTEBK5YuEsBHJF0nAMBrOakcVKa2HWHb-xXb34gygN9z0Wj~Y6iRbdmklBJDIkcJmtRLV~DB3xmV7FY5vTLdiSw~cIkWslr0i6OHbc564PkBeBjJeFBeD7xK8z2tGVUJGqHDHS3kk2BLaJo7fCrGPK3BK9DBAH9BLqae1uyHgH6YIRupbFPne3wEjzafcmttDgVcER4tRH4WNkwG2MuVxy2P6uO990lXi5XFtMcs7X9DdngkIbz9O8yUrFzO16zQ__"
-                            name="Alexandre"
-                            position="Lead Independent Director"
-                            email="Alexandr_develop@gmail.com"
-                            phone="+38 (098) 198 44 24"
-                        />
+                        {users.map((user) => (
+                            <Card key={user.id} {...user} />
+                        ))}
                     </div>
 
-                    <Button text="Show more" style={{ margin: '0 auto' }} />
+                    <Button text="Show more" style={{ margin: '0 auto' }} onClick={usersPaginate} />
                 </div>
 
-                <div className={styles.post_block}>
+                <div className={styles.post_block} ref={signUpRef}>
                     <h1>Working with POST request</h1>
 
                     <form>
@@ -154,6 +168,7 @@ const App = () => {
                         <Button
                             text="Sign up"
                             style={{ marginTop: '50px', width: '100px', marginLeft: 'auto', margin: '50px auto 0' }}
+                            onClick={() => {}}
                         />
                     </form>
                 </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,12 @@ import styles from '@src/components/Button.module.scss';
 type ButtonProps = {
     text: string;
     style?: Object;
+    onClick: () => void;
 };
 
-const Button = ({ text, style }: ButtonProps) => {
+const Button = ({ text, style, onClick }: ButtonProps) => {
     return (
-        <button className={styles.button} style={style}>
+        <button className={styles.button} style={style} onClick={onClick}>
             {text}
         </button>
     );
